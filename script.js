6aa47a3d1144bf39fab9cbb565c725d10fd21703
AileenWang <ainiwang14@gmail.com>
2020-07-28 09:34:27 -0700

    pseudocoding

--- script.js
+++ script.js
@@ -4,4 +4,23 @@
 
 // make button "generate password" that starts process
 
-// make prompt for # of char and confirms for U/l/s///#endregions
+// make prompt for # of char and confirms for U/l/s/#s
+
+// push the user confirm answers to be included in work array
+
+// use math.random and math.floor to randomize; multiply by length of total array of possible char
+
+// use loop to call function number of times of user choice length of pw
+
+// lil tip from wilson:
+// function number(arr) {
+//      for(var i = 0; i < numberArr.length; i++) {
+//          arr.push(numberArr[i]);
+//      };
+//      return arr
+//  };
+// you call that function in your logic I.E.
+// var workshopArr = [];
+// if (userChoiceNumber === true) {
+//   number(workshopArr);
+// };
