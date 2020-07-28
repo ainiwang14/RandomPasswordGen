b506d26016b12bd54f37f456dc81d1e1b173c646
AileenWang <ainiwang14@gmail.com>
2020-07-28 09:14:00 -0700

    barebones

---
+++ script.js
@@ -0,0 +1,7 @@
+// javascript baby!
+
+// make arrays for upper, lower, special, number char, work array, and final password
+
+// make button "generate password" that starts process
+
+// make prompt for # of char and confirms for U/l/s///#endregions
