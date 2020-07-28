585021bbd7a904961526bd2f5e240b780cc25c8e
AileenWang <ainiwang14@gmail.com>
2020-07-28 12:25:15 -0700

    made arrays

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 
 // make arrays for upper, lower, special, number char, work array, and final password
 
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '|', '\\', ';', ':', '?', '-','_', '+', '='];
+var workshopArr = [];
+var finalPass = [];
+
 // make button "generate password" that starts process
 
 // make prompt for # of char and confirms for U/l/s/#s
