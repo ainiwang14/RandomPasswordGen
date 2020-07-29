6766df745a74ced0b66324d4270636389ce04453
AileenWang <ainiwang14@gmail.com>
2020-07-28 21:04:36 -0700

    trying to figure out worrking array function still

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// javascript baby!
-
 // make arrays for upper, lower, special, number char, work array, and final password
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -8,63 +6,75 @@ var special = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', 
 var workshopArr = [];
 var finalPass = [];
 
+var userInputLength
+var userInputUpper
+var userInputLower
+var userInputNumbers
+var userInputSpecial
+
+var randomChar
+
 function passwordOptions() {
 // make prompt for # of char and confirms for U/l/s/#s
-    var userInputLength = parseInt(prompt("How long would you like your password to be? Please enter a number between 8 and 128"));
-        if (!userInputLength) {
-            alert("Ok maybe later");
-            return;
-        }
-    var userInputUpper = confirm("Would you like uppercase letters in your password?");
-    var userInputLower = confirm("Would you like lowercase letters?");
-    var userInputNumbers = confirm("How about numbers?");
-    var userInputSpecial = confirm("And special characters?");
-        if (!userInputUpper && !userInputLower && !userInputNumbers && !userInputSpecial) {
-            alert("you have to pick something");
-            return;
-        }
+    userInputLength = parseInt(prompt("How long would you like your password to be? Please enter a number between 8 and 128"));
+    if (!userInputLength) {
+        alert("Ok maybe later");
+        return;
+    }
+    userInputUpper = confirm("Would you like uppercase letters in your password?");
+    userInputLower = confirm("Would you like lowercase letters?");
+    userInputNumbers = confirm("How about numbers?");
+    userInputSpecial = confirm("And special characters?");
+    if (!userInputUpper && !userInputLower && !userInputNumbers && !userInputSpecial) {
+        alert("you have to pick something");
+        return;
+    }
     
     var passwordOptions = {
-        length: userInputLength;
-        uppercase: userInputUpper;
-        lowercase: userInputLower;
-        numbers: userInputNumbers;
-        special: userInputSpecial;
-    }
+        userInputLength: userInputLength,
+        userInputUpper: userInputUpper,
+        userInputLower: userInputLower,
+        userInputNumbers: userInputNumbers,
+        userInputSpecial: userInputSpecial
+    };
     
     return passwordOptions;
 }
 
-// push the user confirm answers to be included in work array
-function pushToWorkingArr() {
+// // push the user confirm answers to be included in work array
+function toWorkingArr() {
     if (userInputUpper) {
-        workshopArr.push[uppercase]
+        workshopArr.concat[uppercase]
     };
     if (userInputLower) {
-        workshopArr.push[lowercase]
+        workshopArr.concat[lowercase]
     };
     if (userInputNumbers) {
-        workshopArr.push[numbers]
+        workshopArr.concat[numbers]
     };
     if (userInputSpecial) {
-        workshopArr.push[special]
+        workshopArr.concat[special]
     };
 }
 
-// use math.random and math.floor to randomize; multiply by length of total array of possible char
-// use loop to call function number of times of user choice length of pw
-for (var i = 0; i < (userInputLength + 1); i++) {
-    var randomChar = Math.floor(Math.random() * workshopArr.length);
-    finalPass.push(randomChar);
+workshopArr = toWorkingArr(passwordOptions)
+
+// // use math.random and math.floor to randomize; multiply by length of total array of possible char
+// // use loop to call function number of times of user choice length of pw
+function generatePassword(){
+    for (var i = 0; i < (userInputLength); i++) {
+        randomChar = Math.floor(Math.random() * workshopArr.length);
+    }
 }
 
-// Write password to the #password input
+finalPass.push(randomChar);
+
+// // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+    passwordText.textContent = finalPass;
 }
 
-// make button "generate password" that starts process
+// // make button "generate password" that starts process
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener('click', writePassword());
